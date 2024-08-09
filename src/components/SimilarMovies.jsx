c87688c834bc14d2ea8107c47f6327f2da8f0371
Ebrahim Nasser <ebrahimm.nasser@gmail.com>
2024-08-09 03:30:31 +0300

    remove swiperjs and add react slick , edit similarmovies

--- src/components/SimilarMovies.jsx
+++ src/components/SimilarMovies.jsx
@@ -1,65 +1,52 @@
 import Image from "next/image";
 import Link from "next/link";
-// Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-
-import "../app/globals.css";
-
-// import required modules
-import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import Slider from "react-slick";
 
 export default function SimilarMovies({ similarMovies }) {
-  console.log(similarMovies);
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 5000,
+    cssEase: "linear"
+  };
   return (
-    <div className=" flex justify-between items-center flex-col">
+    <div>
       <h2 className=" flex flex-row justify-center w-full mb-5 bg-blue-500 bg-opacity-55 py-10 sm:text-lg md:text-xl lg:text-2xl">
         similar Movies
       </h2>
-      <Swiper
-        spaceBetween={30}
-        centeredSlides={true}
-        autoplay={{
-          delay: 4000,
-          disableOnInteraction: false,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        modules={[Autoplay, Pagination, Navigation]}
-        className="mySwiper"
-      >
-        {similarMovies.slice(0, 7).map((movie, i) => (
-          <SwiperSlide className=" flex" key={i}>
-            <Link
-              href={`/movie/${movie._id}`}
-              key={i}
-              className="relative flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md shadow-slate-200"
-            >
-              <Image
-                width={500}
-                height={250}
-                src={`https://image.tmdb.org/t/p/original/${
-                  movie.poster_path || movie.backdrop_path
-                }`}
-                loading="lazy"
-                alt="poster"
-                className=" rounded-t-lg hover:opacity-75 transition-opacity duration-300:"
-              />
-              <div className="p-6">
-                <h2 className="mb-2 text-xl font-semibold leading-snug truncate text-blue-gray-900 antialiased">
-                  {movie.title}
-                </h2>
-              </div>
-            </Link>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      <div className="slider-container">
+        <Slider {...settings}>
+          {similarMovies?.map((movie) => {
+            return (
+              <Link
+                href={`/movie/${movie._id}`}
+                key={movie._id}
+                className="relative flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md shadow-slate-200"
+              >
+                <Image
+                  width={500}
+                  height={250}
+                  src={`https://image.tmdb.org/t/p/original/${
+                    movie.poster_path || movie.backdrop_path
+                  }`}
+                  loading="lazy"
+                  alt="poster"
+                  className=" rounded-t-lg hover:opacity-75 transition-opacity duration-300:"
+                />
+                <div className="p-6">
+                  <h2 className="mb-2 text-xl font-semibold leading-snug truncate text-blue-gray-900 antialiased">
+                    {movie.title}
+                  </h2>
+                </div>
+              </Link>
+            );
+          })}
+        </Slider>
+      </div>
     </div>
   );
 }
